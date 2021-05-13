38366a3a2028e10786500d4799a5424cfe5568e1
Sascha Block <Block@inztitut.de>
2021-05-13 17:17:51 +0200

    Updated packages    },    "dependencies": {      "@nuxtjs/axios": "^5.13.1",      "@nuxtjs/pwa": "^3.3.5",      "bootstrap": "^4.6.0",      "bootstrap-vue": "^2.21.2",      "core-js": "^3.9.1",      "nuxt": "^2.15.2"    },    "devDependencies": {      "node-sass": "^5.0.0",      "sass-loader": "^11.0.1"    }

--- pages/map.vue
+++ pages/map.vue
@@ -1,19 +1,27 @@
 <template>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin=""/>
+  #mapid { height: 180px; }
   <section class="container">
     <div>
       <h1 class="title">
         Test-Seite
       </h1>
       <h2 class="subtitle">
-        Nur ein Test
+        Ein Test mit OpenMaps
       </h2>
+      <div id="mapid"></div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: "map.vue"
+  name: "map.vue",
+  src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+  crossorigin=""
 }
 </script>
 
