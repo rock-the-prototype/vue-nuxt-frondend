1c226904707b1e1ec45951f6e45f8db1de50fe28
Sascha Block <Block@inztitut.de>
2021-05-16 15:35:36 +0200

    Updated packages    },    "dependencies": {      "@nuxtjs/axios": "^5.13.1",      "@nuxtjs/pwa": "^3.3.5",      "bootstrap": "^4.6.0",      "bootstrap-vue": "^2.21.2",      "core-js": "^3.9.1",      "nuxt": "^2.15.2"    },    "devDependencies": {      "node-sass": "^5.0.0",      "sass-loader": "^11.0.1"    }

--- pages/map.vue
+++ pages/map.vue
@@ -17,10 +17,13 @@
 </template>
 
 <script>
+let integrity;
+let src;
+
 export default {
   name: "map.vue",
-  src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+  src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js",
+  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==",
   crossorigin=""
 }
 </script>
