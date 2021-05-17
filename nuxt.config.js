a12e5ff0bce157a67ac1b279ce6eb0c8105dd933
Sascha Block <Block@inztitut.de>
2021-05-17 22:30:14 +0200

    Add nuxt-leaflet to modules section of nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,8 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    // Simple usage
+    'nuxt-leaflet',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
