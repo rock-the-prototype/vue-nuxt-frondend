23db84f9b521628dd5c3c1942123bdac48ad7326
Sascha Block <Block@inztitut.de>
2021-05-17 22:30:52 +0200

    Add the map to page map.vue

--- pages/map.vue
+++ pages/map.vue
@@ -8,6 +8,14 @@
         Ein Test mit OpenMaps
       </h2>
       <div id="mapid"></div>
+      <div id="map-wrap" style="height: 100vh">
+        <client-only>
+          <l-map :zoom=13 :center="[55.9464418,8.1277591]">
+            <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+            <l-marker :lat-lng="[55.9464418,8.1277591]"></l-marker>
+          </l-map>
+        </client-only>
+      </div>
     </div>
   </section>
 </template>
