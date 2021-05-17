95b72ba32632a2da152d3c1f64fabfbfbe4f3acb
Sascha Block <Block@inztitut.de>
2021-05-17 22:05:59 +0200

    Installation of Vue Leaflet in maps.vue
    webpack.config.js angepasst um:
    import Vue from 'vue';
    import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
    import 'leaflet/dist/leaflet.css';
    
    Vue.component('l-map', LMap);
    Vue.component('l-tile-layer', LTileLayer);
    Vue.component('l-marker', LMarker);

--- pages/map.vue
+++ pages/map.vue
@@ -1,8 +1,4 @@
 <template>
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-        crossorigin=""/>
-  #mapid { height: 180px; }
   <section class="container">
     <div>
       <h1 class="title">
@@ -17,13 +13,23 @@
 </template>
 
 <script>
+// If you need to reference 'L', such as in 'L.icon', then be sure to
+// explicitly import 'leaflet' into your component
+import L from 'leaflet';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 
 export default {
   name: "map.vue",
   src:"https://unpkg.com/leaflet@1.7.1/dist/leaflet.js",
   integrity:"sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==",
-  crossorigin:""
-}
+  crossorigin:"",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+};
+
 </script>
 
 <style scoped>
