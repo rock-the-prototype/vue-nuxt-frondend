ae4177a0c0aea59caa084d53b57df0cdd6e76f2e
AS <as@andre-schuenemann.de>
2021-07-26 20:52:00 +0200

    create page pattern-library

---
+++ pages/pattern-library/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid-container">
+    <section class="library-bar">
+      <a class="header-1" href="#">Layout</a>
+      <a class="header-2" href="#">Colors</a>
+      <a class="header-3" href="#">Fonts</a>
+      <a class="header-4" href="#">Components</a>
+      <a class="header-5" href="#">Media assets</a>
+      <a class="header-6" href="#">tbd</a>
+    </section>
+  </div>
+</template>
