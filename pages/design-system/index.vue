8e4bb47115049484f55d42dc28dbc049af35454a
AS <as@andre-schuenemann.de>
2021-08-16 20:12:20 +0200

    start create design system

---
+++ pages/design-system/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <section>
+      <div class="ds-container">
+        <div class="ds-row">
+          <h1>Design System</h1>
+        </div>
+        <div class="ds-row">
+          <router-link to="/design-system/style-guide">Style Guide</router-link>
+          <router-link to="/design-system/pattern-library"
+            >Pattern Library</router-link
+          >
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<script>
+export default {};
+</script>
---
+++ pages/design-system/pattern-library.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="library-container">
+    <section class="library-bar">
+      <a class="header-1" href="#">all</a>
+      <a class="header-2" href="#">gallery</a>
+      <a class="header-3" href="#">media</a>
+      <a class="header-4" href="#">misc</a>
+      <a class="header-5" href="#">navigation</a>
+      <a class="header-6" href="#">text</a>
+    </section>
+  </div>
+</template>
+<script>
+export default {};
+</script>
